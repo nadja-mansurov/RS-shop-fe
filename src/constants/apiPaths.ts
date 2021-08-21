7ff138087992052281eda48a809a1c400771ba3f
Nadja Mansurov <nadja.mansurov@gmail.com>
2021-08-21 20:15:01 +0200

    add backend API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
+  order: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
+  cart: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
