1da4597be2bc8810a2b99a3e265644d037731d3f
Nadja Mansurov <nadja.mansurov@gmail.com>
2021-08-22 13:34:03 +0200

    Move to Free Tier Account

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
-  order: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
-  import: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
-  bff: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
-  cart: 'https://da3nf9g17h.execute-api.eu-central-1.amazonaws.com/dev',
+  product: 'https://48i2ud2ps6.execute-api.eu-central-1.amazonaws.com/dev',
+  order: 'https://48i2ud2ps6.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://48i2ud2ps6.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://48i2ud2ps6.execute-api.eu-central-1.amazonaws.com/dev',
+  cart: 'https://48i2ud2ps6.execute-api.eu-central-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
